688871487c48e6f3500dd5eb56f459641bd209f2
diego <contact@fartweb.com>
2024-02-16 12:52:13 -0300

    subiendo plomeria cambiando contacto

--- index.js
+++ index.js
@@ -7,10 +7,33 @@ function calculo() {
   parseFloat(calcInput);
   var resultad = calcInput / 10;
 
-  resultado.innerHTML = `<h3>Usted necesita ${resultad} litros de pintura</h3> `;
+  resultado.innerHTML = `<h3 class="resultad">Usted necesita ${resultad} litros de pintura</h3> `;
   console.log(resultad);
 }
 
 function limpiar() {
   resultado.innerHTML = " ";
 }
+
+/*buscador*/
+document.addEventListener("DOMContentLoaded", function () {
+  const searchInput = document.getElementById("searchInput");
+  const productos = document.querySelectorAll(".card.tarjeta");
+
+  searchInput.addEventListener("input", function () {
+    const searchTerm = searchInput.value.toLowerCase();
+    console.log("Search term:", searchTerm);
+
+    productos.forEach(function (producto) {
+      const titulo = producto
+        .querySelector(".card-title")
+        .textContent.toLowerCase();
+
+      if (titulo.includes(searchTerm)) {
+        producto.style.display = "block";
+      } else {
+        producto.style.display = "none";
+      }
+    });
+  });
+});
